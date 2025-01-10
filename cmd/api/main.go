d35a6e61a66d7f4b3bb9d1f888813da62462d187
Edgardo <edgardorodriguezdev@gmail.com>
2025-01-10 17:15:11 -0600

    Project dependencies instalation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,17 @@
-package api
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // Escuchando en el puerto por defecto :8080
+}
